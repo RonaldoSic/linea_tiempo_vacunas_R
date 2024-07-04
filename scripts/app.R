8cfc562e51b5040ee684e8455a0b9bc7ccc5acf8
Ronaldo Sic <rsic@mspas.gob.gt>
2024-07-04 16:19:22 -0600

    Se reconstruyo la tabla ahora solo falta estilizarlo y volver a probar los datos

---
+++ scripts/app.R
@@ -0,0 +1,192 @@
+# rm(list = ls()[grep("^color_", ls())])
+# rm(bd_poblacion)
+
+filtro_departamento <- 3
+anio_cohorte <- 2019
+has_filter <- filtro_departamento >= 1 & filtro_departamento <= 22
+
+bd_poblacion <- df_campania_vacunacion_persona %>% select(
+  FECHA_VACUNACION,
+  DEPARTAMENTO_NOMBRE,
+  DEPARTAMENTO_ID,
+  MUNICIPIO_NOMBRE,
+  MUNICIPIO_ID,
+  TOTAL_NINOS,
+  TOTAL_NINAS
+  ) %>% mutate(
+    POBLACION_TOTAL_VACUNADA = TOTAL_NINOS + TOTAL_NINAS,
+    ANIO = year(FECHA_VACUNACION)
+  ) %>% group_by(FECHA_VACUNACION, DEPARTAMENTO_ID, DEPARTAMENTO_NOMBRE, ANIO) %>% 
+  summarise(
+    POBLACION_TOTAL_VACUNADA = sum(POBLACION_TOTAL_VACUNADA)
+  )
+
+if (has_filter) {
+  bd_poblacion <- bd_poblacion %>% filter(DEPARTAMENTO_ID == filtro_departamento)
+}
+
+bd_poblacion
+
+# ---- TRATAMIENTO DE DATOS DE VACUNAS ----
+
+bd_vacunas <- df_campania_vacunacion_dosis %>% select(
+  FECHA_VACUNACION,
+  DEPARTAMENTO_NOMBRE,
+  DEPARTAMENTO_ID,
+  TOTAL_DOSIS_SPR,
+  TOTAL_DOSIS_OPV,
+  ANIO_COHORTE
+  ) %>% group_by(FECHA_VACUNACION, DEPARTAMENTO_ID, DEPARTAMENTO_NOMBRE) %>%
+  summarise(
+    TOTAL_DOSIS_SPR = sum(TOTAL_DOSIS_SPR),
+    TOTAL_DOSIS_OPV = sum(TOTAL_DOSIS_OPV)
+  )
+
+# bd_vacunas <- df_campania_vacunacion_dosis %>% pivot_wider(
+#   names_from = ANIO_COHORTE,
+#   values_from = c(TOTAL_DOSIS_SPR, TOTAL_DOSIS_OPV)
+# )
+
+
+if (has_filter) {
+  bd_vacunas <- bd_vacunas %>% filter(DEPARTAMENTO_ID == filtro_departamento)
+}
+  
+bd_vacunas
+
+
+# bd_nacidos_ine <- df_nacidos_vivos_ine %>% select(
+#   ANIO, 
+#   DEPARTAMENTO,
+#   IDDEP,
+#   POBLACION
+# ) %>% group_by(ANIO, IDDEP, DEPARTAMENTO) %>% 
+#   summarise(
+#     POBLACION_TOTAL = sum(POBLACION),
+#   )
+
+bd_nacidos_ine <- df_nacidos_vivos_ine %>% pivot_wider(
+  names_from = ANIO,
+  values_from = POBLACION
+) %>% select(
+  IDDEP,
+  DEPARTAMENTO,
+  MUNICIPIO,
+  IDMUN,
+  `2019`,
+  `2020`,
+  `2021`,
+  `2022`
+) %>% rename(
+  POBLACION_2019 = `2019`,
+  POBLACION_2020 = `2020`,
+  POBLACION_2021 = `2021`,
+  POBLACION_2022 = `2022`
+) %>% mutate(
+  POBLACION_TOTAL = POBLACION_2019 + POBLACION_2020 + POBLACION_2021 + POBLACION_2022
+) %>% select(
+  IDDEP,
+  DEPARTAMENTO,
+  POBLACION_2019,
+  POBLACION_2020,
+  POBLACION_2021,
+  POBLACION_2022,
+  POBLACION_TOTAL
+) %>% group_by(IDDEP, DEPARTAMENTO) %>% 
+  summarise(
+    POBLACION_TOTAL = sum(POBLACION_TOTAL)
+  )
+
+bd_nacidos_ine
+
+if (has_filter) {
+  bd_nacidos_ine <- bd_nacidos_ine %>% filter(IDDEP == filtro_departamento)
+} else {
+  # sumar todos los departamentos para obtener la población total aunque no se pueda agrupar por departamento ni por id
+  bd_nacidos_ine_nacional <- bd_nacidos_ine %>% summarise(
+    IDDEP = 0,
+    POBLACION_TOTAL = sum(POBLACION_TOTAL, na.rm = TRUE)
+  ) %>% group_by(IDDEP) %>% 
+    summarise(
+      POBLACION_TOTAL = sum(POBLACION_TOTAL)
+    )
+}
+
+bd_nacidos_ine
+bd_nacidos_ine_nacional
+
+
+tabla_vacunados <- bd_poblacion %>% left_join(bd_vacunas, 
+                                              by = c("FECHA_VACUNACION", "DEPARTAMENTO_ID", "DEPARTAMENTO_NOMBRE")) %>% 
+  mutate(
+    # si tiene filtro se usa la población total de nacidos vivos del INE, de lo contrario se usa la población total de nacidos vivos del INE nacional
+    COBERTURA_SPR = ifelse(has_filter, round((TOTAL_DOSIS_SPR / bd_nacidos_ine$POBLACION_TOTAL) * 100, 2), 
+                           round((TOTAL_DOSIS_SPR / bd_nacidos_ine_nacional$POBLACION_TOTAL) * 100, 2)),
+    COBERTURA_OPV = ifelse(has_filter, round((TOTAL_DOSIS_OPV / bd_nacidos_ine$POBLACION_TOTAL) * 100, 2), 
+                           round((TOTAL_DOSIS_OPV / bd_nacidos_ine_nacional$POBLACION_TOTAL) * 100, 2))
+  ) %>% select(
+    DEPARTAMENTO_ID,
+    DEPARTAMENTO_NOMBRE,
+    FECHA_VACUNACION,
+    POBLACION_TOTAL_VACUNADA,
+    TOTAL_DOSIS_SPR,
+    TOTAL_DOSIS_OPV,
+    COBERTURA_SPR,
+    COBERTURA_OPV
+  )
+
+tabla_vacunados
+
+# ---- CONSTRUYENDO LA GRAFICA ----
+
+grafica_avance_campania_vacunacion <- plot_ly(
+  tabla_vacunados, x = ~FECHA_VACUNACION, y = ~TOTAL_DOSIS_SPR,
+  type = 'bar', name = 'Vacuna SPR', 
+  hovertemplate = "%{y}",
+  marker = list(color = color_principal)
+) %>% add_trace(
+  y = ~TOTAL_DOSIS_OPV, name = 'Vacuna OPV',
+  hovertemplate = "%{y}",
+  marker = list(color = color_secundario)
+) %>% add_trace(
+  y = ~COBERTURA_SPR, name = 'Cobertura SPR (%)',
+  mode = 'lines+markers', yaxis = 'y2',
+  hovertemplate = "%{y}%",
+  line = list(color = color_terciario, width = 2, dash = 'dashdot'),
+  marker = list(color = color_disenio)
+) %>% add_trace(
+  y = ~COBERTURA_OPV, name = 'Cobertura OPV (%)',
+  mode = 'lines+markers', yaxis = 'y2',
+  hovertemplate = "%{y}%",
+  line = list(color = color_disenio, width = 2, dash = 'dashdot'),
+  marker = list(color = color_disenio)
+) %>% layout(
+  title = 'Avance de la campaña de vacunación',
+  xaxis = list(title = 'Fecha de vacunación', showgrid = FALSE, zeroline = FALSE, fixedrange = TRUE, 
+               tickformat = '%d-%M-%Y'),
+  yaxis = list(title = 'Total de dosis aplicadas', rangemode = 'tozero', showgrid = FALSE, 
+               zeroline = FALSE, fixedrange = TRUE, tickformat = ',d'),
+  yaxis2 = list(title = 'Cobertura (%)', overlaying = 'y', side = 'right', rangemode = 'tozero', 
+                showgrid = FALSE, zeroline = FALSE, fixedrange = TRUE),
+  barmode = 'group',
+  titlefont = list(size = 16),
+  margin = list(l = 50, r = 50, b = 50, t = 50),
+  hovermode = 'x unified',
+  paper_bgcolor = color_fondo,
+  plot_bgcolor = color_fondo
+) %>% config(
+  locale = 'es',
+  displayModeBar = FALSE,
+  modeBarButtonsToRemove = c("select2d", "lasso2d", "zoomIn2d", "zoomOut2d", "zoom", "pan", "autoscale"),
+  toImageButtonOptions = list(
+    format = "png",
+    filename = "comparacion_vacunas_diaria",
+    height = 600,
+    width = 1600,
+    scale = 1
+  )
+)
+
+grafica_avance_campania_vacunacion
+
+
--- scripts/server.R
+++ scripts/server.R
@@ -33,61 +33,18 @@ shinyServer(function(input, output) {
 
   # Grafica de linea de tiempo por departamento, DDRISS y si es en total para todos --------- 
   output$avance_campania_vacunacion <- renderPlotly({
-    # obtiene la informacion de la poblacion por departamento 
     new_bd_poblacion <- reactive_df_campania_persona() %>% 
+      group_by(FECHA_VACUNACION = as.Date(FECHA_VACUNACION)) %>%
       select(
-        DEPARTAMENTO_NOMBRE,
-        TOTAL_NINAS,
-        TOTAL_NINOS,
-        FECHA_VACUNACION
-      ) %>% gruop_by(
-        DEPARTAMENTO_NOMBRE,
-        FECHA_VACUNACION
-      )%>% mutate(
-        POBLACION = TOTAL_NINAS + TOTAL_NINOS
-      ) %>% summarise(
-        POBLACION = sum(POBLACION)
+        DEPARTAMENTO = DEPARTAMENTO_NOMBRE,
+        MUNICIPIO = MUNICIPIO_NOMBRE
       )
     
-    # obtiene la informacion de la vacunacion por departamento
-    new_bd_vacunacion <- reactive_df_campania_dosis () %>% 
-      mutate(
-        TOTAL_DOSIS = TOTAL_DOSIS_SPR + TOTAL_DOSIS_OPV,
-      ) %>% sumarise(
-        TOTAL_DOSIS = sum(TOTAL_DOSIS),
-        TOTAL_DOSIS_SPR = sum(TOTAL_DOSIS_SPR),
-        TOTAL_DOSIS_OPV = sum(TOTAL_DOSIS_OPV)
-      )
-      select (
-        ANIO_COHORTE,
-        DEPARTAMENTO_NOMBRE,
-        AREA_SALUD,
-        TOTAL_DOSIS_SPR,
-        TOTAL_DOSIS_OPV,
-        FECHA_VACUNACION
-      ) %>% group_by(
-        ANIO_COHORTE,
-        DEPARTAMENTO_NOMBRE,
-        AREA_SALUD, 
-        FECHA_VACUNACION
-      )
-      
-      #Creando la tabla con la informacion de la vacunacion y la poblacion
-      tabla_cacunados <- merge(new_bd_poblacion, new_bd_vacunacion, by = "DEPARTAMENTO_NOMBRE")
-      
-      # creando la grafic
-      
-
     
     
   })
   
   output$avance_campania_vacunacion_ddriss <- renderPlotly({
-    df <- reactive_df_campania_persona()
-    df <- df %>% group_by(FECHA_VACUNACION) %>% summarise(Total = sum(TOTAL))
-    df <- df %>% arrange(FECHA_VACUNACION)
-    plot_ly(df, x = ~FECHA_VACUNACION, y = ~Total, type = 'scatter', mode = 'lines', name = 'Total', line = list(color = 'blue')) %>%
-      layout(title = "Avance de la campaña de vacunación por DDRISS", xaxis = list(title = "Fecha de vacunación"), yaxis = list(title = "Total de vacunados"))
   })
   
   
